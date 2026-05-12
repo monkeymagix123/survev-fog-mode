bcd495b1e032c6bcfb66905530f224d2db629b6b
sus <iamsuchabozo12345@gmail.com>
2026-05-11 18:15:07 -0700

    added fogMode config file

---
+++ fogMode.ts
@@ -0,0 +1,14 @@
+
+const fogConfig = {
+    visibility: {
+        hideObjectsBehindOpaqueObstacles: true,
+        fogMode: {
+            ambientDarkness: 1,
+            // lightStrength: 1.15,
+            lightStrength: 3,
+            lightFalloff: 2,
+            lightFalloffStart: 4,
+            enableShadows: true,
+        },
+    },
+}
